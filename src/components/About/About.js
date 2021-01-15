26e0bea8e2b099157f92e0d91295cd5dfb80dc35
enyelsequeira <enyelsequeira@hotmail.com>
2021-01-15 21:43:59 +0000

    fixing small tweak before full on refactor

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -9,11 +9,9 @@ const About = () => (
     <div className="about__text">
       <h2 className="about__text-title">About the author</h2>
       <p className="about__text-para">
-        This block describes the project author. Here you should indicate your name, what you do, and which development technologies you know.
+        I am a web developer that comes from a teaching and Business background. Through my past experiences, I can balance the need of users and businesses to understand their pain points and how I can contribute as a Front-end developer and solve their problems
       </p>
-      <p className="about__text-para">You can also talk about your experience with Practicum, what you learned there, and how you can help potential customers.</p>
-      <p className="about__text-para">Text goes here</p>
-      <p className="about__text-para">Text goes here</p>
+      <p className="about__text-para">I&#39;ve worked with a wide range of people from different backgrounds and demographics. This has enabled me to adapt and become flexible in the workplace.</p>
 
     </div>
   </section>
